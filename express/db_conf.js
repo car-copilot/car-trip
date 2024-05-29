9e9803286c91c6d7cb37cca62c699d9536ee8031
garatquent <garatquent@eisti.eu>
2024-05-29 14:35:51 +0200

    basic API to get trips

---
+++ express/db_conf.js
@@ -0,0 +1,35 @@
+const Pool = require('pg').Pool
+require('dotenv').config()
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+
+const getTripsName = async () => {
+    try {
+        return await new Promise(function (resolve, reject) {
+        pool.query("SELECT * FROM car, trip WHERE trip.car_id = car.car_id", (error, results) => {
+            if (error) {
+            reject(error);
+            }
+            if (results && results.rows) {
+            resolve(results.rows);
+            } else {
+            reject(new Error("No results found"));
+            }
+        });
+        });
+    } catch (error_1) {
+        console.error(error_1);
+        throw new Error("Internal server error");
+    }
+};
+
+module.exports = {
+    getTripsName
+};
