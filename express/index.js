02bf495803cbc0e6798c74fc832e203eb587ca5c
garatquent <garatquent@eisti.eu>
2024-05-29 15:09:03 +0200

    update cors

--- express/index.js
+++ express/index.js
@@ -6,7 +6,7 @@ const db = require('./db_conf')
 
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
